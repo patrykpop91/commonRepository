72752e10ed3d34ba31b5debf2056440334b71a3c
Krzysztof Ozurkiewicz <k.ozurkiewicz@execoz.pl>
2017-10-25 23:44:44 +0200

    Added Gulp and Sass to ozi_theme

---
+++ wp-content/themes/ozi_theme/gulpfile.js
@@ -0,0 +1,18 @@
+require('es6-promise').polyfill();
+var gulp          = require('gulp');
+var sass          = require('gulp-sass');
+var autoprefixer  = require('gulp-autoprefixer');
+
+gulp.task('sass', function() {
+  return gulp.src('./sass/**/*.scss')
+  .pipe(sass())
+  .pipe(autoprefixer())
+  .pipe(gulp.dest('./'))
+});
+
+
+gulp.task('watch', function() {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass', 'watch']);
--- wp-content/themes/ozi_theme/index.php
+++ wp-content/themes/ozi_theme/index.php
@@ -1,1 +1,13 @@
-<h1>Działa, kurczę pieczone</h1>
+<!DOCTYPE html>
+<html>
+<head>
+<link href="<?php echo get_stylesheet_uri(); ?>" rel="stylesheet" type="text/css"> 
+<title>ozi_theme</title>
+</head>
+<body>
+
+<h1>Chyba działa</h1>
+<p>Kurcze pieczone!!!</p>
+
+</body>
+</html>
