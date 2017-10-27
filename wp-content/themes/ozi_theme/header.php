72fb8b5da0aae779ee602a171f328cd02cbc0125
Krzysztof Ozurkiewicz <k.ozurkiewicz@execoz.pl>
2017-10-27 15:09:07 +0200

    Start prepare header

---
+++ wp-content/themes/ozi_theme/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang='pl'>
+<head>
+    <meta charset="utf-8">
+<link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet" type="text/css"> 
+<title><?php bloginfo('name'); ?></title>
+<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/custom.js"></script>
+</head>
--- wp-content/themes/ozi_theme/index.php
+++ wp-content/themes/ozi_theme/index.php
@@ -1,13 +1,33 @@
-<!DOCTYPE html>
-<html>
-<head>
-<link href="<?php echo get_stylesheet_uri(); ?>" rel="stylesheet" type="text/css"> 
-<title>ozi_theme</title>
-</head>
-<body>
-
-<h1>Chyba działa</h1>
-<p>Kurcze pieczone!!!</p>
-
-</body>
-</html>
+<?php get_header(); ?>
+<div class="container">
+    <header>
+        <div class="header-content-wrapper">
+            <nav>
+                <div class="header-content-logo">
+                    <img src="<?php bloginfo('template_directory') ?>/img/logo.png" alt="logo">
+                </div>
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Blog</li>
+                    <li>Services</li>
+                    <li>Pages</li>
+                    <li>Contact Us</li>
+                </ul>
+            </nav>
+            <div class="header-content-text">
+                <div class="text-box">
+                    <div class="text-box-hello">
+                        <img src="<?php bloginfo('template_directory') ?>/img/hello.png" alt="hello">
+                    </div>
+                    <div class="text-box-header">
+                        <h2>My Name is <span>John Doe</span></h2>
+                        <h3>Ceo | !st Agency</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+                    </div>
+                </div>
+                <button>Click here</button>
+            </div>
+        </div>
+    </header>
+</div>
