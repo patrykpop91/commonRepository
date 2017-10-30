8340cc76f6db0ae1dc75006c8bafbd6270fde57f
Krzysztof Ozurkiewicz <k.ozurkiewicz@execoz.pl>
2017-10-30 12:46:48 +0100

    Header 80% complete :)

--- wp-content/themes/ozi_theme/gulpfile.js
+++ wp-content/themes/ozi_theme/gulpfile.js
@@ -2,17 +2,33 @@ require('es6-promise').polyfill();
 var gulp          = require('gulp');
 var sass          = require('gulp-sass');
 var autoprefixer  = require('gulp-autoprefixer');
+var livereload = require('gulp-livereload');
 
 gulp.task('sass', function() {
   return gulp.src('./sass/**/*.scss')
   .pipe(sass())
   .pipe(autoprefixer())
   .pipe(gulp.dest('./'))
+  .pipe(livereload());
+});
+
+
+// PHP
+gulp.task('php', function() {
+  return gulp.src([
+      './index.php'
+  ])
+  .pipe(livereload());
 });
 
 
 gulp.task('watch', function() {
+  livereload.listen();
   gulp.watch('./sass/**/*.scss', ['sass']);
+  // Watch HTML and livereload
+  gulp.watch('./index.php', ['php']);
 });
 
-gulp.task('default', ['sass', 'watch']);
+
+
+gulp.task('default', ['sass', 'watch', 'php']);
--- wp-content/themes/ozi_theme/index.php
+++ wp-content/themes/ozi_theme/index.php
@@ -1,12 +1,12 @@
 <?php get_header(); ?>
 <div class="container">
     <header>
-        <div class="header-content-wrapper">
+        <div class="header-content-wrapper clearfix">
             <nav>
                 <div class="header-content-logo">
                     <img src="<?php bloginfo('template_directory') ?>/img/logo.png" alt="logo">
                 </div>
-                <ul>
+                <ul class="clearfix">
                     <li>Home</li>
                     <li>About Us</li>
                     <li>Blog</li>
@@ -25,8 +25,12 @@
                         <h3>Ceo | !st Agency</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
                     </div>
+                    <button>Click here</button>
                 </div>
-                <button>Click here</button>
+                <div class="img-box">
+                    <img src="<?php bloginfo('template_directory') ?>/img/ludzik.png" alt="ludzik">
+                </div>
+                
             </div>
         </div>
     </header>
