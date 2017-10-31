a3b5ea57cc143011391ee570d8128b0a835e4d1e
Krzysztof Ozurkiewicz <k.ozurkiewicz@execoz.pl>
2017-10-31 11:14:54 +0100

    3 sections added

--- wp-content/themes/ozi_theme/header.php
+++ wp-content/themes/ozi_theme/header.php
@@ -2,6 +2,7 @@
 <html lang='pl'>
 <head>
     <meta charset="utf-8">
+<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
 <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet" type="text/css"> 
 <title><?php bloginfo('name'); ?></title>
 <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/custom.js"></script>
--- wp-content/themes/ozi_theme/index.php
+++ wp-content/themes/ozi_theme/index.php
@@ -21,7 +21,7 @@
                         <img src="<?php bloginfo('template_directory') ?>/img/hello.png" alt="hello">
                     </div>
                     <div class="text-box-header">
-                        <h2>My Name is <span>John Doe</span></h2>
+                        <h2>My Name is <span style="color:#0081ff;">John Doe</span></h2>
                         <h3>Ceo | !st Agency</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
                     </div>
@@ -53,5 +53,28 @@
             <button>Click here</button>
         </div>
         
+    </section>
+    <section class="second-section">
+        <div class="header-center-wrapper-second-section">
+            <img src="<?php bloginfo('template_directory') ?>/img/services.png" alt="services">
+            <h2>What I do...</h2>
+        </div>
+        <div class="content-horizontal-second-section">
+            <ul>
+                <li><img src="<?php bloginfo('template_directory') ?>/img/ikon1-services.png" alt=""><h3>Lorem ipsum dolor sit amet</h3><p>Lorem ipsum dolor sit amet, consectetur adipicing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud</p>
+                </li>
+                <li><img src="<?php bloginfo('template_directory') ?>/img/ikon2-services.png" alt=""><h3>Lorem ipsum dolor sit amet</h3><p>Lorem ipsum dolor sit amet, consectetur adipicing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud</p>
+                </li>
+                <li><img src="<?php bloginfo('template_directory') ?>/img/ikon3-services.png" alt=""><h3>Lorem ipsum dolor sit amet</h3><p>Lorem ipsum dolor sit amet, consectetur adipicing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                    </p>
+                </li>
+            </ul>       
+                </div>
     </section>
 </div>
